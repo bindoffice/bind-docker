a790c75afca207dfef5d0c2ae9959fa99ed320d7
solos <lxl1217@gmail.com>
2026-03-31 11:01:16 +0800

    fix setup db
    fix setup db

--- office/crdb/setup_db.sh
+++ office/crdb/setup_db.sh
@@ -2,6 +2,11 @@
 echo Wait for servers to be up
 sleep 1
 
+set -a # 将所有变量自动导出
+source ./.env
+set +a
+
+
 CRDB_ADDR='127.0.0.1:26257';
 HOSTPARAMS="--host ${CRDB_ADDR} --insecure";
 SQL="/cockroach/cockroach.sh sql $HOSTPARAMS";
