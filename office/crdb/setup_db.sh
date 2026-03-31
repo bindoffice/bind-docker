09105b4c0cc09d8d67ac73900c3def42a8ea38e7
solos <lxl1217@gmail.com>
2026-03-31 10:53:01 +0800

    db name use vars
    db name use vars

--- office/crdb/setup_db.sh
+++ office/crdb/setup_db.sh
@@ -3,7 +3,6 @@ echo Wait for servers to be up
 sleep 1
 
 CRDB_ADDR='127.0.0.1:26257';
-DB_NAME='hedwi_selfhost';
 HOSTPARAMS="--host ${CRDB_ADDR} --insecure";
 SQL="/cockroach/cockroach.sh sql $HOSTPARAMS";
 
