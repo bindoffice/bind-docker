09f69d8a6d32350ea0f47183b94d76676730ac05
solos <lxl1217@gmail.com>
2026-04-23 10:08:01 +0800

    update script
    update script

--- office/bin/remove.sh
+++ office/bin/remove.sh
@@ -5,7 +5,7 @@ source ./.env ;
 set +a;
 
 
-List=(bind-inbox bind-task bind-search bind-meetserver bind-smtp bind-imap bind-office-api bind-mqtt bind-mcp)
+List=(bind-inbox bind-task bind-search bind-meetserver bind-smtp bind-imap bind-office-api bind-mqtt bind-mcp bind-static)
 
 for name in ${List[@]};
 do
--- office/bin/update.sh
+++ office/bin/update.sh
@@ -13,3 +13,4 @@ docker pull "$DOCKER_REGISTRY"bindoffice/bind-task:latest
 docker pull "$DOCKER_REGISTRY"bindoffice/bind-search:latest
 docker pull "$DOCKER_REGISTRY"bindoffice/bind-mqtt:latest
 docker pull "$DOCKER_REGISTRY"bindoffice/bind-mcp:latest
+docker pull "$DOCKER_REGISTRY"bindoffice/bind-static:latest
